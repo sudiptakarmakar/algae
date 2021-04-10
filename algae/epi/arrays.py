90e653f995b028d1a40b712e420e311cca9a8951
Sudipta Karmakar <sudiptakarmakar@users.noreply.github.com>
2021-04-10 11:23:20 -0400

    Majority of the remaining problems from Arrays (#5)
    * Add answers till arrays Q11
    
    * Set linter max line length 100
    
    * Add matrix rotation solutions
    
    * Ignore certain flake8 errors
    
    * Add pascals triangle solution

--- algae/epi/arrays.py
+++ algae/epi/arrays.py
@@ -1,6 +1,10 @@
+import bisect
+import collections
+import itertools
 import math
+import random
 
-from typing import List, Any
+from typing import List, Any, Generator
 
 
 def dutch_national_flag(numbers: List[int], index: int):
@@ -47,7 +51,6 @@ def multiply_arbitrary_precision_int(one: List[int], two: List[int]):
     the inputs are [1,9,3,7,0,7,7,2,1] and <-7,6,1,8,3,8,2,5,7,2,8,7>, your
     function should return (-1,4,7,5,7,3,9,5,2,5,8,9,6,7,6,4,1,2,9,2,7).
     """
-    pass
 
 
 def advance_through_array(steps: List[int]):
@@ -98,7 +101,6 @@ def buy_sell_stock_twice(prices):
     buying and selling a share at most twice. The second buy must be made on
     another date after the first sale.
     """
-    pass
 
 
 def enumerate_primes(limit: int):
@@ -142,41 +144,223 @@ def permute_array_elements(elements: List[Any], permutation: List[int]):
     return result
 
 
-def compute_next_permutation():
-    pass
+def compute_next_permutation(permutation: List[int]):
+    """Write a program that takes as input a permutation, and returns the next
+    permutation under dictionary ordering. If the permutation is the last
+    permutation, return the empty array.
+    For example, if the input is (1, 0, 3, 2) your function should return
+    (1, 2, 0, 3). If the input is (3, 2, 1, 0), return ()
+    """
+    # find inflexion point
+    prev = permutation[-1]
+    index = len(permutation) - 2
+    while index >= 0:
+        current = permutation[index]
+        if current < prev:
+            break
+        prev = current
+        index -= 1
+    if index < 0:
+        permutation.clear()
+        return
+
+    # index is right before the last non increasing suffix, swap it with the
+    # smallest number larger than it to its right
+    small_index = index
+    while index < len(permutation) and permutation[index] >= permutation[small_index]:
+        index += 1
+    permutation[small_index], permutation[index - 1] = (
+        permutation[index - 1],
+        permutation[small_index],
+    )
+
+    # reverse the rightmost sorted subsection to order it ascending
+    left = small_index + 1
+    right = len(permutation) - 1
+    while left < right:
+        permutation[left], permutation[right] = permutation[right], permutation[left]
+        left += 1
+        right -= 1
 
 
-def sample_offline_data():
-    pass
+def sample_offline_data(elements: List[int], size: int):
+    """Implement an algorithm that takes as input an array of distinct elements
+    and a size, and returns a subset of the given size of the array elements.
+    All subsets should be equally likely. Return the result in input array itself.
+    """
+    for i in range(size):
+        index = random.randint(i, len(elements) - 1)
+        elements[i], elements[index] = elements[index], elements[i]
 
 
-def sample_oinline_data():
-    pass
+def sample_online_data(sequence: Generator, k: int):
+    """Design a program that takes as input a size k, and reads packets from a
+    sequence generator, continuously maintaining a uniform random subset of size
+    k of the read packets.
+    """
+    sample_k = [next(sequence) for _ in range(k)]
+    count = k
+
+    for element in sequence:
+        index = random.randint(0, count)
+        if index < k:
+            sample_k[index] = element
+        count += 1
+
+    return sample_k
 
 
-def compute_random_permutation():
-    pass
+def compute_random_permutation(size: int):
+    """Design an algorithm that creates uniformly random permutations of
+    (0, 1,..., n-1). You are given a random number generator that returns
+    integers in the set (0, 1,..., n-1) with equal probability; use as few calls
+    to it as possible.
+    """
+    elements = list(range(size))
+    for i in range(size):
+        index = random.randint(i, size - 1)  # randint is inclusive of range
+        elements[i], elements[index] = elements[index], elements[i]
+    return elements
 
 
-def compute_random_subset():
-    pass
+def compute_random_subset(n: int):
+    """Write a program that takes as input a positive integer n and a size
+    k < n, and returns a size-k subset of {0,1, 2,..., n - 1}. The subset should
+    be represented as an array. All subsets should be equally likely and, in
+    addition, all permutations of elements of the array should be equally
+    likely. You may assume you have a function which takes as input a non-negative
+    integer t and returns an integer in the set {0, 1, . . . , t - 1} with uniform
+    probability.
+    """
 
 
-def generate_nonuniform_random_number():
-    pass
+def generate_nonuniform_random_number(numbers: List[int], probabilities: List[float]) -> int:
+    """You are given n numbers and corresponding probabilities P0, P1,..., Pn-1
+    - which sum up to 1. Given a random number generator that produces values in
+    [0,1] uniformly, how would you generate one of the n numbers according to
+    the specified probabilities?
+    For example, if the numbers are 3, 5, 7, 11, and the probabilities are 9/18,
+    6/18, 2/18, 1/18, then in 1000000 calls to your program, 3 should appear
+    roughly 500000 times, 5 should appear roughly 333333 times, 7 should appear
+    roughly 111111 times, and 11 should appear roughly 55555 times.
+    """
+    prefix_sum = list(itertools.accumulate(probabilities))
+    weighted_index = bisect.bisect(prefix_sum, random.random())
+    return numbers[weighted_index]
 
 
-def sudoku_checker():
-    pass
+def sudoku_checker(sudoku: List[List[int]]) -> bool:
+    """Check whether a 9 X 9 2D array representing a partially completed Sudoku
+    is valid. Specifically, check that no row, column, or 3 X 3 2D subarray
+    contains duplicates. A 0-value in the 2D array indicates that entry is blank;
+    every other entry is in [1,9].
+    """
+    blk_size = int(math.sqrt(len(sudoku)))
+    memo = set()
+    for row, row_values in enumerate(sudoku):
+        for col, value in enumerate(row_values):
+            if value == 0:
+                continue
+            r = (f"row-{row}", value)
+            c = (f"col-{col}", value)
+            b = (f"block-{row//blk_size}-{col//blk_size}", value)
+            if r in memo or c in memo or b in memo:
+                return False
+            memo.update([r, c, b])
+    return True
 
 
-def spiral_ordering_2d_array():
-    pass
+def sudoku_checker_one_liner(sudoku: List[List[int]]) -> bool:
+    """Check whether a 9 X 9 2D array representing a partially completed Sudoku
+    is valid. Specifically, check that no row, column, or 3 X 3 2D subarray
+    contains duplicates. A 0-value in the 2D array indicates that entry is blank;
+    every other entry is in [1,9].
+    """
+    blk_size = int(math.sqrt(len(sudoku)))
+    return (
+        max(
+            collections.Counter(
+                val
+                for r, row in enumerate(sudoku)
+                for c, el in enumerate(row)
+                if el != 0
+                for val in (("r", r, el), ("c", c, el), ("b", r // blk_size, c // blk_size, el))
+            ).values(),
+            default=0,
+        )
+        <= 1
+    )
 
 
-def rotate_2d_array():
-    pass
+def spiral_ordering_2d_array(matrix: List[List[int]]) -> List[int]:
+    """Write a program which takes an nxn 2D array and returns the spiral
+    ordering of the array.
+    """
+    result = []
+    row = 0
+    col = 0
+    width = len(matrix) - 1
+    while width > 0:
+        for row_step, col_step in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+            for _ in range(width):
+                result.append(matrix[row][col])
+                row += row_step
+                col += col_step
+        row += 1
+        col += 1
+        width -= 2
+    if width == 0:
+        result.append(matrix[row][col])
+    return result
 
 
-def compute_rows_pascals_triangle():
-    pass
+def rotate_2d_array(matrix: List[List[int]]):
+    """Write a function that takes as input an n X n 2D array, and rotates the
+    array by 90 degrees clockwise.
+    """
+    row = 0
+    col = 0
+    width = len(matrix) - 1
+    max_width = width
+
+    while width > 0:
+        memo = matrix[row][col : (col + width)]
+        for row_step, col_step in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+            for idx in range(width):
+                dest_row = col
+                dest_col = max_width - row
+                matrix[dest_row][dest_col], memo[idx] = memo[idx], matrix[dest_row][dest_col]
+                row += row_step
+                col += col_step
+        row += 1
+        col += 1
+        width -= 2
+
+
+def rotate_2d_array_improved(matrix: List[List[int]]):
+    """Write a function that takes as input an n X n 2D array, and rotates the
+    array by 90 degrees clockwise.
+    """
+    width = len(matrix) - 1
+    for i in range(len(matrix) // 2):
+        for j in range(i, width - i):
+            matrix[i][j], matrix[~j][i], matrix[~i][~j], matrix[j][~i] = (
+                matrix[~j][i],
+                matrix[~i][~j],
+                matrix[j][~i],
+                matrix[i][j],
+            )
+
+
+def compute_rows_pascals_triangle(n: int):
+    """Write a program which takes as input a nonnegative integer n and returns
+    the first n rows of Pascal's triangle.
+    """
+    result = []
+    row = [1]
+    result.append(row)
+    for _ in range(1, n):
+        next_row = [1] + [a + b for a, b in zip(row, row[1:])] + [1]
+        result.append(next_row)
+        row = next_row
+    return result
